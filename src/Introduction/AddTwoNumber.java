e16bc5838405af3641704e0067c2164593d4307e
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-08-19 08:26:06 +0200

    Add Two Number

---
+++ src/Introduction/AddTwoNumber.java
@@ -0,0 +1,9 @@
+package Introduction;
+public class AddTwoNumber {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
