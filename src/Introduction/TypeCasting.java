1965a658c0e4eaa159d1653c90cabd9889778e0e
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-08-20 09:44:45 +0200

    Casting

---
+++ src/Introduction/TypeCasting.java
@@ -0,0 +1,13 @@
+package Introduction;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+//        int a = 5;
+//        char ch = (char) a;// you have to convert to char type
+//        int b = (int)6.7;// 6.7 by default double we need to convert it
+        System.out.println(4+4);//int
+        System.out.println(4+4.5);//double
+        System.out.println(4.1+4.4);//double
+
+    }
+}
