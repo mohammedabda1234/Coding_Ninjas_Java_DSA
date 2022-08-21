7915880554d296bf557763bbf30b6dc8e1f43b56
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-08-21 17:04:34 +0200

    pattern 7

---
+++ src/Patterns/CharacterPattern2.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class CharacterPattern2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = 1;
+        while (i <= n){
+            int j = 1;
+            char ch = (char) ('A' + i - 1);
+            while (j <= n){
+                System.out.print(ch);
+                j++;
+                ch++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
