6bedc8acf0f27515f66e9dad24db08a39e6fc3e8
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-08-23 20:40:29 +0200

    Square Root

---
+++ src/ForLoop/SquareRoot.java
@@ -0,0 +1,11 @@
+package ForLoop;
+
+import java.util.Scanner;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.println(Math.sqrt(n));
+    }
+}
