71d52696fd165bb4f70ac67347d80c462ece0930
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-08-25 16:56:50 +0200

    duplicate element

--- src/Array_1/FindUnique.java
+++ src/Array_1/FindUnique.java
@@ -1,5 +1,7 @@
 package Array_1;
 
+import java.util.Arrays;
+
 public class FindUnique {
     public static int find(int[] arr){
         int count ;
@@ -14,10 +16,28 @@ public class FindUnique {
         }
         return 0;
     }
-
+    public static int duplicateNumber(int[] arr){
+        int i,j,c;
+        for(i=0;i<arr.length;i++){
+            c=0;
+            for(j=0;j<arr.length;j++){
+                if(arr[i]==arr[j])
+                    c++;
+            }
+            if(c==2){
+                return  arr[i];
+            }
+        }
+        return 0;
+    }
+    static  void  print(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
     public static void main(String[] args) {
         int[] arr = {1,2,3,1,2,3,5,6};
-        int result = find(arr);
+        int result = duplicateNumber(arr);
         System.out.println(result);
     }
 }
