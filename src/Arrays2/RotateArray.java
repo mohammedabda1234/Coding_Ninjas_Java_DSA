4497a78c38287d64d4e1a93483945a88448bf313
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-08-27 20:53:58 +0200

    rotated arrays

--- src/Arrays2/RotateArray.java
+++ src/Arrays2/RotateArray.java
@@ -7,7 +7,25 @@ public class RotateArray {
         }
         System.out.println();
     }
+    public static void rotate(int[] arr,int d){
+        int n = arr.length;
+        int[] temp = new int[n];
+        int k = 0;
+        for (int i = d; i < n; i++) {
+            temp[k] = arr[i];
+            k++;
+        }
+        for (int i = 0; i < d; i++) {
+            temp[k] = arr[i];
+            k++;
+        }
+        for (int i = 0; i < n; i++) {
+           arr[i] = temp[i];
+        }
+    }
     public static void main(String[] args) {
-
+        int[] arr = {5,6,7,8,9,0,2,3};
+        rotate(arr,2);
+        print(arr);
     }
 }
