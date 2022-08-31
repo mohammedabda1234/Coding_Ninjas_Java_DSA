981b774c0942d36807e19187be71d9e5f32cb27e
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-08-31 09:34:17 +0200

    function with 2D arrays

--- src/Array2D/FuntionIn2D.java
+++ src/Array2D/FuntionIn2D.java
@@ -2,8 +2,8 @@ package Array2D;
 
 import java.util.Scanner;
 
-public class InputAndPrinting {
-    public static void main(String[] args) {
+public class FuntionIn2D {
+    public static int[][] takeInput(){
         Scanner s = new Scanner(System.in);
         System.out.println("Enter number of rows");
         int rows = s.nextInt();
@@ -12,17 +12,23 @@ public class InputAndPrinting {
         int[][] arr = new int[rows][col];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < col; j++) {
-                System.out.println("Enter element of rows "+(i+1)+" columns "+(j+1));
+                System.out.println("Enter row no "+(i+1)+" columns no "+(j+1));
                 arr[i][j] = s.nextInt();
             }
         }
-        System.out.println("our array is ");
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < col; j++) {
-                System.out.print("|"+arr[i][j]+"");
+        return  arr;
+    }
+    public static void  print2D(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print("|"+arr[i][j]+" ");
                 System.out.print("|");
             }
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+        int[][] arr = takeInput();
+        print2D(arr);
     }
 }
