95335e3678759424f86c0bf961d387c399023c19
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-01 15:07:20 +0200

    constructor

--- src/OOP1/Student.java
+++ src/OOP1/Student.java
@@ -2,16 +2,26 @@ package OOP1;
 
 public class Student {
    public String name;
-    //public that can access to all package in the same project ;
-   private int rollNumber;//private int rollNumber;
-   //private  can access in same class that have this variable
-    String depart;
-    //default  can access in all class in same package
+   private int rollNumber;
+   public Student(String name,int rollNumber){//constructor with parameter
+       this.name = name;
+       this.rollNumber = rollNumber;
+   }
+   public Student(){//default constructor
+
+   }
+   public Student(String name){// constructor with one parameter
+       this.name = name;
+       this.rollNumber = 20;
+   }
+   public void print(){
+       System.out.println(name+" "+rollNumber);
+   }
     public int getRollNumber(){//getter help us to access with private entity
         return rollNumber;
     }
     public void setRollNumber(int num){//setter
-        if (num <= 0){//help us to make control to our data;
+        if (num >= 0){//help us to make control to our data;
             return;
         }
         rollNumber = num;
--- src/OOP1/StudentUse.java
+++ src/OOP1/StudentUse.java
@@ -2,14 +2,9 @@ package OOP1;
 
 public class StudentUse {
     public static void main(String[] args) {
-        Student s1 = new Student();//object of student class;
-        Student s2 = new Student();
-        System.out.println(s1);
-        s1.name = "Mohamed";//to access entity of class ;
-        s1.setRollNumber(20);
-        System.out.println(s1.name+" "+s1.getRollNumber());
-        s2.name = "Ali";
-        s2.setRollNumber(40);
-        System.out.println(s2.name+" "+s2.getRollNumber());
+        Student s1 = new Student("Ali",20);//object of student class;
+        Student s2 = new Student("Mohamed");
+        s1.print();
+        s2.print();
     }
 }
