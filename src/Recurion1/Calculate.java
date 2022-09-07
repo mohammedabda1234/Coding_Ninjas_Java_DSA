c5cb6268a3fa54545f356208ff925c4f898a3eba
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-07 08:27:16 +0200

    print first N natural number

--- src/Recurion1/Calculate.java
+++ src/Recurion1/Calculate.java
@@ -14,8 +14,16 @@ public class Calculate {
         }
         return x * power(x,n -1);
     }
+    public static void printN(int num){
+        if (num == 0){
+            return;
+        }
+        printN(num - 1);
+        System.out.print(num+" ");
+    }
     public static void main(String[] args) {
-        System.out.println(power(2,8));
-        System.out.println(count(1090));
+//        System.out.println(power(2,8));
+//        System.out.println(count(1090));
+        printN(10);
     }
 }
