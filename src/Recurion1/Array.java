8cbe2f927d40b41f02f16bd6eee39af1f3145a0d
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-07 14:03:01 +0200

    check sorted array

--- src/Recurion1/Array.java
+++ src/Recurion1/Array.java
@@ -15,8 +15,19 @@ public class Array {
         boolean isSmallArraySorted = isSorted(smallArray);
         return isSmallArraySorted;
     }
+    public static boolean isSorted(int[] arr,int startIndex){
+        if (startIndex == arr.length -1){
+            return true;
+        }
+        if (arr[startIndex]>arr[startIndex +1]){
+            return false;
+        }
+        boolean isSmallArraySorted = isSorted(arr,startIndex +1);
+        return isSmallArraySorted;
+
+    }
     public static void main(String[] args) {
         int[] arr = {2,4,5};
-        System.out.println(isSorted(arr));
+        System.out.println(isSorted(arr,0));
     }
 }
