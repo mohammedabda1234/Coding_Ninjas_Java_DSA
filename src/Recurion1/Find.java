541310a29569b46a76c599337c55edc15de8ab25
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-07 14:38:05 +0200

    last index in array

--- src/Recurion1/Find.java
+++ src/Recurion1/Find.java
@@ -14,9 +14,24 @@ public class Find {
     public static int firstIndex(int[] arr,int num) {
         return firstIndex(arr, 0, arr.length-1, num);
     }
-
+    public static int lastIndex(int[] arr,int x,int si){
+        if (si == arr.length ){
+            return -1;
+        }
+        int k = lastIndex(arr,x,si +1);
+        if (k != -1){
+            return k;
+        }
+        else {
+            if (arr[si] == x){
+                return si;
+            }else
+                return -1;
+        }
+    }
     public static void main(String[] args) {
         int[] arr = {5,7,8,9,3,2,1,4,5,6,7};
         System.out.println(firstIndex(arr,7));
+        System.out.println(lastIndex(arr,7,0));
     }
 }
