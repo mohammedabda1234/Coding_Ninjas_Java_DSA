70e5047fd1d3d0b09ee57eec440add20a4b582b3
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-07 13:49:29 +0200

    check number in array

--- src/Recurion1/Sum.java
+++ src/Recurion1/Sum.java
@@ -8,10 +8,22 @@ public class Sum {
         return (sum(arr,n-1)+arr[n - 1]);
     }
 
+    public static boolean checkNumber(int input[], int x) {
+        return checkNumber(input,x,0);
+    }
+    private static boolean checkNumber(int input[],int x,int startIndex){
+        if(startIndex==input.length)
+            return false;
+        if(input[startIndex]==x)
+            return true;
+        return checkNumber(input,x,startIndex+1);
+
+    }
     public static void main(String[] args) {
         int[] arr = {1,2,3};
         System.out.println(sum(arr,arr.length ));
 //        System.out.println(sum(arr));
+        System.out.println(checkNumber(arr,2));
     }
 
 //    public static int sum(int input[]) {
