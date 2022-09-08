302ef8c775073d7f006d8c4c4244c565b4bd0f61
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-08 08:09:43 +0200

    remove X

--- src/Recursion2/ReplaceChar.java
+++ src/Recursion2/ReplaceChar.java
@@ -13,7 +13,18 @@ public class ReplaceChar {
             return str.charAt(0) + smallOutput;
         }
     }
+    public static String removeX(String str){
+        if (str.length() == 0){
+            return str;
+        }
+        if (str.charAt(0) == 'x'){
+            return removeX(str.substring(1));
+        }else {
+            return str.charAt(0)+removeX(str.substring(1));
+        }
+    }
     public static void main(String[] args) {
         System.out.println(replace("hello",'l','h'));
+        System.out.println(removeX("relex"));
     }
 }
