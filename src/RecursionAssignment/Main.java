c9f7996d5a5275315006f8552d5054cdce26a1a1
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-08 19:24:36 +0200

    count zeros

--- src/RecursionAssignment/Main.java
+++ src/RecursionAssignment/Main.java
@@ -44,10 +44,36 @@ public class Main {
             }
         }
     }
+
+    public static int countZerosRec(int input){//ans one
+        if (input == 0) {
+            return 1;
+        } else if (input <= 9) {
+            return 0;
+        } else {
+            return ((input % 10 == 0) ? 1 : 0) + countZerosRec(input / 10);
+        }
+    }
+
+    public static int countZeros(int num){//ans two
+        if (num == 0){//spacial case to handel zeros
+            return 1;
+        }
+        if (num < 10){//base case
+            return 0;
+        }
+        else if (num % 10 == 0){
+            return 1 + countZeros(num/10);
+        }
+        return countZeros(num/10);
+    }
+
     public static void main(String[] args) {
         System.out.println(findGeometricSum(4));
         System.out.println(isStringPalindrome("hello"));
         System.out.println(sumOfDigits(435));
         System.out.println(multiplyTwoIntegers(4,5));
+        System.out.println(countZerosRec(90876));
+        System.out.println(countZeros(409098708));
     }
 }
