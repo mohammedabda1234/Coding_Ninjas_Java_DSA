3eaf1f2e7251ec8937160c4dda9a43c11f60cadd
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-08 19:08:34 +0200

    Sum of digits (recursive)

--- src/RecursionAssignment/Main.java
+++ src/RecursionAssignment/Main.java
@@ -26,8 +26,15 @@ public class Main {
 
 
     }
+    public static int sumOfDigits(int input){
+        if (input == 0)
+            return 0;
+        return (input % 10 + sumOfDigits(input / 10));
+
+    }
     public static void main(String[] args) {
         System.out.println(findGeometricSum(4));
         System.out.println(isStringPalindrome("hello"));
+        System.out.println(sumOfDigits(435));
     }
 }
