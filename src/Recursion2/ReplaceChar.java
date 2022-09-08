a74d3e0ef46eea599ec58cccdecc5fad6af47759
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-08 08:30:05 +0200

    replace pi

--- src/Recursion2/ReplaceChar.java
+++ src/Recursion2/ReplaceChar.java
@@ -23,8 +23,23 @@ public class ReplaceChar {
             return str.charAt(0)+removeX(str.substring(1));
         }
     }
+    public static String replacePi(String str){
+       if (str.length() <= 1){
+           return str;
+       }
+       if (str.charAt(0) == 'p' && str.charAt(1) == 'i'){
+           String output = replacePi(str.substring(2));
+           return "3.14"+output;
+       }
+       else {
+           String output = replacePi(str.substring(1));
+           return str.charAt(0)+output;
+       }
+
+    }
     public static void main(String[] args) {
         System.out.println(replace("hello",'l','h'));
         System.out.println(removeX("relex"));
+        System.out.println(replacePi("pifacepi"));
     }
 }
