3ad38969e90c901db5ecbef7dffe3ea3959b2d7e
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-09 10:09:55 +0200

    Array Equilibrium Index

---
+++ src/Recursion2/Equilibrium.java
@@ -0,0 +1,24 @@
+package Recursion2;
+
+public class Equilibrium {
+    public static int arrayEquilibriumIndex(int[] arr){
+        int i, j;
+        int leftsum, rightsum;
+        int n = arr.length;
+        for (i = 0; i < n; ++i) {
+            leftsum = 0;
+            for (j = 0; j < i; j++)
+                leftsum += arr[j];
+            rightsum = 0;
+            for (j = i + 1; j < n; j++)
+                rightsum += arr[j];
+            if (leftsum == rightsum)
+                return i;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,7,8,9,1,1,1};
+        System.out.println(arrayEquilibriumIndex(arr));
+    }
+}
