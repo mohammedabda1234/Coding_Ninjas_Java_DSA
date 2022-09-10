70b1a267cb6c9b22c7456b4939c361ae11dd572e
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-10 09:13:21 +0200

    pair sum

--- src/TimeComplexity/Duplicate.java
+++ src/TimeComplexity/Duplicate.java
@@ -9,9 +9,24 @@ public class Duplicate {
         }
         return sum-(n*(n+1)/2);
     }
+    public static int pairSum(int[] arr, int num) {
+        int count=0;
+        for (int i=0;i<arr.length;i++)
+        {
+            for (int j=i+1;j<arr.length;j++)
+            {
+                if (arr[i]+arr[j]==num)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
-        int[] arr ={0 ,7 ,2 ,5 ,4 ,7 ,1 ,3 ,6};
+        int[] arr ={0 ,7 ,8,2 ,5 ,4 ,7 ,1 ,3 ,6};
         System.out.println(findDuplicate(arr));
+        System.out.println(pairSum(arr,10));
 
     }
 }
