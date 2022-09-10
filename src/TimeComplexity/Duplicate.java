913384fbfc079d1b995a2bbd2502b75a384b2de7
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-10 09:24:42 +0200

    Triplet sum

--- src/TimeComplexity/Duplicate.java
+++ src/TimeComplexity/Duplicate.java
@@ -1,5 +1,7 @@
 package TimeComplexity;
 
+import java.util.Arrays;
+
 public class Duplicate {
     public static int findDuplicate(int[] arr) {
         int n=arr.length-2;
@@ -23,10 +25,62 @@ public class Duplicate {
         }
         return count;
     }
+    public static int tripletSum(int[] arr, int num) {
+
+        Arrays.sort(arr);
+        int n = arr.length;
+
+        int numTriplets = 0;
+
+        for (int i=0; i<n; i++) {
+            int pairSum = num - arr[i];
+            int numPairs = 0;
+
+            int start = i + 1;
+            int stop = n - 1;
+
+            while (start < stop) {
+                if (arr[start] + arr[stop] < pairSum) {
+                    start++;
+                } else if (arr[start] + arr[stop] > pairSum) {
+                    stop--;
+                } else {
+                    if (arr[start] == arr[stop]) {
+                        int totalCount = (stop - start) + 1;
+                        numPairs += (totalCount * (totalCount - 1) / 2);
+                        break;
+                    }
+
+                    int tempI = start + 1;
+                    int tempJ = stop - 1;
+
+                    while (tempI <= tempJ && arr[start] == arr[tempI]) {
+                        tempI++;
+                    }
+
+                    while (tempI <= tempJ && arr[stop] == arr[tempJ]) {
+                        tempJ--;
+                    }
+
+                    int totalElementFromStart = (tempI - start);
+                    int totalElementFromEnd = stop - tempJ;
+
+                    numPairs += (totalElementFromStart * totalElementFromEnd);
+
+                    start = tempI;
+                    stop = tempJ;
+                }
+            }
+            numTriplets += numPairs;
+        }
+        return numTriplets;
+
+    }
     public static void main(String[] args) {
         int[] arr ={0 ,7 ,8,2 ,5 ,4 ,7 ,1 ,3 ,6};
         System.out.println(findDuplicate(arr));
         System.out.println(pairSum(arr,10));
+        System.out.println(tripletSum(arr,20));
 
     }
 }
