bc402a0498d628819705cc7157b5086c66f00f9b
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-10 16:09:57 +0200

    Does s contain t ?

---
+++ src/Test1DSA/Code.java
@@ -0,0 +1,31 @@
+package Test1DSA;
+
+public class Code {
+    public static boolean checkSequence(String a, String b) {
+        return checkSequence(a,b,0,0);
+    }
+    public static boolean checkSequence(String a, String b, int a_idx, int b_idx)
+    {
+        if (b_idx==b.length())
+        {
+            return true;
+        }
+        if (a_idx==a.length())
+        {
+            return false;
+        }
+        if (b.charAt(b_idx)==a.charAt(a_idx))
+        {
+            return checkSequence(a,b,a_idx+1,b_idx+1);
+        }
+        else
+        {
+            return checkSequence(a,b,a_idx+1,b_idx);
+        }
+    }
+    public static void main(String[] args) {
+        String str = "hello world";
+        System.out.println(checkSequence(str,"wor"));
+
+    }
+}
