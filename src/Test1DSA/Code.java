3a9664c773c1a32261cb353eec6ef0b46a9582f5
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-10 16:15:23 +0200

    Split Array

--- src/Test1DSA/Code.java
+++ src/Test1DSA/Code.java
@@ -37,6 +37,34 @@ public class Code {
         return ans;
     }
 
+    public static boolean splitArray(int input[]) {
+
+        return splitArray(input,0,0,0);
+    }
+
+    public static boolean splitArray(int arr[], int si, int lsum, int rsum)
+    {
+
+        if (si == arr.length)
+        {
+            return lsum == rsum;
+        }
+
+        if (arr[si] % 5 == 0)
+        {
+            lsum += arr[si];
+        }
+        else if (arr[si] % 3 == 0)
+        {
+            rsum += arr[si];
+        }
+        else
+        {
+            return splitArray(arr,si+1, lsum+arr[si], rsum)  || splitArray(arr,si+1,lsum,rsum+arr[si]);
+        }
+
+        return splitArray(arr,si+1,lsum,rsum);
+    }
     public static void main(String[] args) {
         String str = "hello world";
         System.out.println(checkSequence(str,"wor"));
