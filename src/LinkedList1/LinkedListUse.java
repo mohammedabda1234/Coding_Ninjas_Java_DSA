2cbc4cddea6d94543fbb7253385fd16069e82b53
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-12 13:37:29 +0200

    Length Of LL

--- src/LinkedList1/LinkedListUse.java
+++ src/LinkedList1/LinkedListUse.java
@@ -32,10 +32,20 @@ public class LinkedListUse {
             temp = temp.next;
         }
     }
+    public static int length(Node<Integer>head){
+        Node<Integer>temp = head;
+        int count = 0;
+        while (temp != null){
+            count++;
+            temp =temp.next;
+        }
+        return count;
+    }
     public static void main(String[] args) {
         Node<Integer> head = createLinkedList();
         increment(head);
         print(head);
+        System.out.println(length(head));
 //        Node<Integer> n1 = new Node<>(10);
 //        System.out.println(n1);
 //        System.out.println(n1.data);
