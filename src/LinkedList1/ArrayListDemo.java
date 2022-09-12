15801873f3cd72e821cb29750331e718c4df7e44
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-12 06:42:57 +0200

    arraylist

--- src/LinkedList1/ArrayListDemo.java
+++ src/LinkedList1/ArrayListDemo.java
@@ -8,9 +8,21 @@ public class ArrayListDemo {
         arr.add(10);
         arr.add(20);
         arr.add(30);
+        arr.add(40);
+        arr.add(60);
+        arr.add(80);
         arr.add(1,50);
+        arr.remove(1);
         System.out.println(arr.get(1));
         System.out.println(arr.size());
+        System.out.println(arr.get(1));
 
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.println(arr.get(i));
+        }
+        System.out.println();
+        for (int i:arr){
+            System.out.println(i);
+        }
     }
 }
