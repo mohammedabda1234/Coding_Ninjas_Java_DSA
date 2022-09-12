b180309e31c2cd665bba6490d2f02fd872434095
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-12 06:56:28 +0200

    remove duplicate element

---
+++ src/LinkedList1/Remove.java
@@ -0,0 +1,23 @@
+package LinkedList1;
+
+import java.util.ArrayList;
+
+public class Remove {
+    public static ArrayList<Integer> removeDuplicates(int[] arr){
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i-1]){
+                result.add(arr[i]);
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10,10,20,20,30,40,30,10};
+        ArrayList<Integer> result = removeDuplicates(arr);
+        for (int i : result){
+            System.out.println(i);
+        }
+    }
+}
