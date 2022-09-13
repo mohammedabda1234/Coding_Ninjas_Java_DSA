6e0d071461d6a041130e3ba4c7dd32c93ad8c97b
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-13 08:32:50 +0200

    take input

--- src/LinkedList1/LinkedListUse.java
+++ src/LinkedList1/LinkedListUse.java
@@ -63,17 +63,19 @@ public class LinkedListUse {
     public static Node<Integer> takeInput(){
         Scanner s = new Scanner(System.in);
         int data = s.nextInt();
-        Node<Integer> head = null;
+        Node<Integer> head = null,tail = null;
         while (data != -1){
             Node<Integer> currentNode = new Node<>(data);
             if (head == null){
                 head = currentNode;
+                tail = currentNode;
             }else {
-                Node<Integer> tail = head;
-                while (tail.next != null){
-                    tail = tail.next;
-                }
+//                Node<Integer> tail = head;
+//                while (tail.next != null){
+//                    tail = tail.next;
+//                }
                 tail.next = currentNode;
+                tail = currentNode;
             }
             data  = s.nextInt();
         }
