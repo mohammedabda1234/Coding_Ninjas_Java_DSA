b7e9bfdf4e54e3e3d2fc766681c14f2e02602d22
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-13 10:04:24 +0200

    AppendLastNToFirst

--- src/LinkedList1/LinkedListUse.java
+++ src/LinkedList1/LinkedListUse.java
@@ -135,12 +135,58 @@ public class LinkedListUse {
         else
             return -1;
     }
+    public static Node<Integer> appendLastNToFirst(Node<Integer> head, int n) {
+        int totalelements = 0;
+        Node<Integer> count =head;
+        if(head==null)
+        {
+            return null;
+        }
+        if(n==0)
+        {
+            return count;
+        }
+        while(count!=null)
+        {
+            totalelements++;
+            count=count.next;
+        }
+        int sub = totalelements-n; //5-3=2
+        Node<Integer> nh = head;
+        int c=0;
+        while(c<sub)
+        {
+            c++;
+            nh=nh.next;
+        }
+        Node<Integer> newhead = nh;
+
+        Node<Integer> prev = newhead;
+        while(prev.next!=null)
+        {
+            prev=prev.next;
+        }
+        prev.next = head;
+
+        Node<Integer> br = head;
+        int co = 0;
+        while(co<sub-1)
+        {
+            co++;
+            br = br.next;
+        }
+        br.next=null;
+
+        return newhead;
+    }
     public static void main(String[] args) {
         Node<Integer> head = takeInput();//createLinkedList();
         head = insert(head,10,3);
 //        increment(head);
         print(head);
         head = deleteNode(head,0);
+        print(head);
+        head = appendLastNToFirst(head,2);
         print(head);
         System.out.println(findNode(head,3));
 //        System.out.println(length(head));
