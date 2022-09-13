dbc2d30fcea9c882c6769cea472ed0eb8a8f50e9
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-13 13:27:41 +0200

    Eliminate duplicates from LL

--- src/LinkedList1/LinkedListUse.java
+++ src/LinkedList1/LinkedListUse.java
@@ -179,10 +179,33 @@ public class LinkedListUse {
 
         return newhead;
     }
+    public static Node<Integer> removeDuplicates(Node<Integer> head) {
+
+        if(head==null)
+            return head;
+        if(head.next==null)
+            return head;
+        Node<Integer> t1=head,t2=head.next;
+        Node<Integer> finalhead=head;
+        while(t2!=null){
+            if(t1.data.equals(t2.data))
+            {
+                t2=t2.next;
+            }
+            else{
+                t1.next=t2;
+                t1=t2;
+            }
+        }
+
+        t1.next=null;
+        return finalhead;
+    }
     public static void main(String[] args) {
         Node<Integer> head = takeInput();//createLinkedList();
         head = insert(head,10,3);
 //        increment(head);
+        head = removeDuplicates(head);
         print(head);
         head = deleteNode(head,0);
         print(head);
