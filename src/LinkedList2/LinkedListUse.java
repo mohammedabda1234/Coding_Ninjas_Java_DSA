92d53bc5e8da84f78b23cc4b49fc8fd0fe93cd14
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-14 20:47:41 +0200

    Swap two Nodes of LL

--- src/LinkedList2/LinkedListUse.java
+++ src/LinkedList2/LinkedListUse.java
@@ -480,6 +480,61 @@ public class LinkedListUse {
         return head;
 
     }
+    public static Node<Integer> swapNodes(Node<Integer> head, int i, int j) {
+        if(i==j)
+        {
+            return head;
+        }
+        if(i>j)
+        {
+            int t = i;
+            i=j;
+            j=t;
+        }
+
+
+        Node<Integer> p1=null,p2=null,n1=null,n2=null;
+        Node<Integer> temp = head;
+        Node<Integer> tail = null;
+        int count = 0;
+        while(temp!=null)
+        {
+            if(count==i)
+            {
+
+                p1= tail;
+                n1=temp;
+            }
+            else if(count==j)
+            {
+                p2=tail;
+                n2=temp;
+            }
+            tail=temp;
+            temp=temp.next;
+            count++;
+
+        }
+        if(p1==null)
+        {
+            head=n2;
+        }
+        else
+        {
+            p1.next=n2;
+            p2.next=n1;
+
+        }
+
+        Node<Integer> nextelem = n2.next;
+        n2.next=n1.next;
+        n1.next=nextelem;
+
+
+
+        return head;
+    }
+
     public static void main(String[] args) {
         Node<Integer> head1 = takeInput();//createLinkedList();
         head1 = skipMdeleteN(head1,2,4);
