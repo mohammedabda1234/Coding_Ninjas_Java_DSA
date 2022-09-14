eccda9aa621cd448573608316eaeddcda44c761c
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-14 19:20:24 +0200

    merge two sorted ll

--- src/LinkedList2/LinkedListUse.java
+++ src/LinkedList2/LinkedListUse.java
@@ -333,15 +333,53 @@ public class LinkedListUse {
             while (fast.next != null && fast.next.next != null){
                 slow = slow.next;
                 fast = fast.next.next;
-
         }
         return slow;
     }
+    public static Node<Integer>merge(Node<Integer>head1,Node<Integer>head2){
+        if (head1 == null){
+            return  head2;
+        }
+        if (head2 == null){
+            return head1;
+        }
+        Node<Integer>t1 = head1,t2 = head2;
+        Node<Integer> head = null,tail = null;
+        if (t1.data <= t2.data){
+            head = t1;
+            tail = t1;
+            t1 = t1.next;
+        }else {
+            head = t2;
+            tail = t2;
+            t2 = t2.next;
+        }
+        while (t1 != null && t2 != null){
+            if (t1.data <= t2.data){
+                tail.next =t1;
+                tail =t1;
+                t1 = t1.next;
+            }else {
+                tail.next = t2;
+                tail = t2;
+                t2 = t2.next;
+            }
+        }
+        if (t1 != null){
+            tail.next = t1;
+        }else {
+            tail.next =t2;
+        }
+        return  head;
+    }
     public static void main(String[] args) {
-        Node<Integer> head = takeInput();//createLinkedList();
+        Node<Integer> head1 = takeInput();//createLinkedList();
+        Node<Integer> head2 = takeInput();
+        Node<Integer> head = merge(head1,head2);
+        printR(head);
 //        head = reverse_I(head);
-        head = midPoint(head);
-        System.out.println(head.data);
+//        head = midPoint(head);
+//        System.out.println(head.data);
 //        DoubleNode ans =reverseRBetter(head);
 //        printR(head);
 //        printReverse(head);
