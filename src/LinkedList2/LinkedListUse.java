5e93ee5de626eb6f98a79111e176a87cd991b1ab
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-14 20:50:09 +0200

    kReverse

--- src/LinkedList2/LinkedListUse.java
+++ src/LinkedList2/LinkedListUse.java
@@ -535,6 +535,45 @@ public class LinkedListUse {
         return head;
     }
 
+    public static Node<Integer> kReverse(Node<Integer> head, int k) {
+        if(head==null)
+            return head;
+        if(head.next==null)
+            return head;
+        if(k==0)
+            return head;
+        Node<Integer> h1=head,h2,t1=head;
+        int count=1;
+        while(count<k && t1.next!=null)
+        {
+            t1=t1.next;
+            count++;
+        }
+
+        h2=t1.next;
+        t1.next=null;
+
+
+        DoubleNode ans=reversePart(h1);
+        Node<Integer> secondHead=kReverse(h2,k);
+        ans.tail.next=secondHead;
+        return ans.head;
+
+    }
+    private static DoubleNode reversePart(Node<Integer> head)
+    {
+        if(head==null || head.next==null)
+        { DoubleNode ans=new DoubleNode();
+            ans.head=head;
+            ans.tail=head;
+            return ans;}
+
+        DoubleNode ans=reversePart(head.next);
+        ans.tail.next=head;
+        head.next=null;
+        ans.tail=ans.tail.next;
+        return ans;
+    }
     public static void main(String[] args) {
         Node<Integer> head1 = takeInput();//createLinkedList();
         head1 = skipMdeleteN(head1,2,4);
