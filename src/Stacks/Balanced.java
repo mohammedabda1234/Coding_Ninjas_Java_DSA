95b46110dbfcbf20bbbeae949e1850700b0b2382
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-15 15:47:29 +0200

    Minimum bracket Reversal

--- src/Stacks/Balanced.java
+++ src/Stacks/Balanced.java
@@ -99,13 +99,53 @@ public class Balanced {
         return output;
     }
 
+    static int countBracketReversals(String input)
+    {
+        // Step 1: Initialize a stack of char type and ans as 0.
+        Stack<Character> st = new Stack<Character>();
+        int ans = 0;
+
+        // Step 2: Run a loop for each character of the string
+        for (int i = 0; i < input.length(); i++) {
+
+            // Step 2.1: If ' { ' encountered push it to the
+            // stack
+            if (input.charAt(i) == '{')
+                st.add(input.charAt(i));
+                // Step 2.2: If ' } ' is encountered
+            else {
+                // Step 2.2.1: If stack has a '{' present for
+                // '}' encountered, pop from the stack.
+                if (!st.isEmpty())
+                    st.pop();
+
+                    // Step 2.2.2: If stack is empty, change '}' to
+                    // '{' and push it to stack and increment ans by
+                    // 1
+                else {
+                    st.add('{');
+                    ans++;
+                }
+            }
+        }
+        // Step 3: if stack size is odd return -1.
+        if (st.size() % 2 != 0)
+            return -1;
+        // Step 4: Increment ans by ( stackSize/2 ).
+        ans += st.size() / 2;
+
+        return ans;
+    }
     public static void main(String[] args) {
 //        String s = "({[]})";
 //        boolean re = isValid(s);
 //        System.out.println(re);
 
-        String s = "a+(b)+c \n";
-        boolean re = checkRedundancy(s);
+//        String s = "a+(b)+c \n";
+//        boolean re = checkRedundancy(s);
+//        System.out.println(re);
+        String str = "[{{()}}]";
+        int re = countBracketReversals(str);
         System.out.println(re);
     }
 }
