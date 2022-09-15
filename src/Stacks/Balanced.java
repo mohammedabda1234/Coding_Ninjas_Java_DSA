234e63c6c60c82b97b2733aad8a214444a8bb0f1
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-15 15:35:45 +0200

    Stock Span

--- src/Stacks/Balanced.java
+++ src/Stacks/Balanced.java
@@ -79,6 +79,26 @@ public class Balanced {
         }
         return false;
     }
+    public static int[] stockSpan(int[] price) {
+        Stack<Integer> stk = new Stack<>();
+        int n = price.length;
+        int[] output = new int[n];
+        stk.push(0);
+        output[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            while (!stk.isEmpty() && price[stk.peek()] < price[i]) {
+                stk.pop();
+            }
+            if (stk.isEmpty()) {
+                output[i] = i + 1;
+            } else {
+                output[i] = i - stk.peek();
+            }
+            stk.push(i);
+        }
+        return output;
+    }
+
     public static void main(String[] args) {
 //        String s = "({[]})";
 //        boolean re = isValid(s);
