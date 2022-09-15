b51bbb916bf1e69e1d81d72644f34c090aaed401
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-15 15:28:26 +0200

    Check redundant brackets

--- src/Stacks/Balanced.java
+++ src/Stacks/Balanced.java
@@ -45,10 +45,47 @@ public class Balanced {
             input.push(k);
         }
     }
-    public static void main(String[] args) {
-        String s = "({[]})";
-        boolean re = isValid(s);
-        System.out.println(re);
 
+    static boolean checkRedundancy(String expression) {
+        // create a stack of characters
+        Stack<Character> st = new Stack<>();
+        char[] str = expression.toCharArray();
+        // Iterate through the given expression
+        for (char ch : str) {
+            // if current character is close parenthesis ')'
+            if (ch == ')') {
+                char top = st.peek();
+                st.pop();
+                // If immediate pop have open parenthesis '('
+                // duplicate brackets found
+                boolean flag = true;
+                while (top != '(') {
+                    // Check for operators in expression
+                    if (top == '+' || top == '-'
+                            || top == '*' || top == '/') {
+                        flag = false;
+                    }
+                    // Fetch top element of stack
+                    top = st.peek();
+                    st.pop();
+                }
+                // If operators not found
+                if (flag == true) {
+                    return true;
+                }
+            } else {
+                st.push(ch); // push open parenthesis '(',
+            }                // operators and operands to stack
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+//        String s = "({[]})";
+//        boolean re = isValid(s);
+//        System.out.println(re);
+
+        String s = "a+(b)+c \n";
+        boolean re = checkRedundancy(s);
+        System.out.println(re);
     }
 }
