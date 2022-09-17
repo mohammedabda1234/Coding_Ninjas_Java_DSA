781cba736aeae145e63e5971b2f18c349c8f9e5b
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-17 06:53:40 +0200

    Create binary tree node

---
+++ src/BinaryTreesI/BinaryTreeUse.java
@@ -0,0 +1,17 @@
+package BinaryTreesI;
+
+public class BinaryTreeUse {
+    public static void main(String[] args) {
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(1);
+
+        BinaryTreeNode<Integer> rootLeft = new BinaryTreeNode<>(2);
+        BinaryTreeNode<Integer> rootRight = new BinaryTreeNode<>(3);
+        root.left = rootLeft;
+        root.right = rootRight;
+
+        BinaryTreeNode<Integer> twoRight = new BinaryTreeNode<>(2);
+        BinaryTreeNode<Integer> threeLeft = new BinaryTreeNode<>(3);
+        rootLeft.right = twoRight;
+        rootRight.left = threeLeft;
+    }
+}
