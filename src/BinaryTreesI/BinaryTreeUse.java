627dcca1c23551f0633315673a9f89fcf489bf30
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-17 08:25:25 +0200

    print tree recursively 1

--- src/BinaryTreesI/BinaryTreeUse.java
+++ src/BinaryTreesI/BinaryTreeUse.java
@@ -1,6 +1,20 @@
 package BinaryTreesI;
 
 public class BinaryTreeUse {
+    public static void printTree(BinaryTreeNode<Integer>root){
+        if (root == null){
+            return;
+        }
+        System.out.println(root.data);
+        printTree(root.left);
+        printTree(root.right);
+//        if (root.left != null){
+//            printTree(root.left);
+//        }
+//        if (root.right != null){
+//            printTree(root.right);
+//        }
+    }
     public static void main(String[] args) {
         BinaryTreeNode<Integer> root = new BinaryTreeNode<>(1);
 
@@ -8,10 +22,11 @@ public class BinaryTreeUse {
         BinaryTreeNode<Integer> rootRight = new BinaryTreeNode<>(3);
         root.left = rootLeft;
         root.right = rootRight;
+        printTree(root);
 
-        BinaryTreeNode<Integer> twoRight = new BinaryTreeNode<>(2);
-        BinaryTreeNode<Integer> threeLeft = new BinaryTreeNode<>(3);
-        rootLeft.right = twoRight;
-        rootRight.left = threeLeft;
+//        BinaryTreeNode<Integer> twoRight = new BinaryTreeNode<>(4);
+//        BinaryTreeNode<Integer> threeLeft = new BinaryTreeNode<>(5);
+//        rootLeft.right = twoRight;
+//        rootRight.left = threeLeft;
     }
 }
