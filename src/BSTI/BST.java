f9b6bc5915170be9d4a0d253fef9c2bb3db8fff6
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-19 08:19:10 +0200

    Check if binary tree is BST?

--- src/BSTI/BST.java
+++ src/BSTI/BST.java
@@ -68,6 +68,41 @@ public class BST {
             printBtnK1AndK2(root.right,k1,k2);
         }
     }
+    public static int minimum(BinaryTreeNode<Integer> root) {
+        if (root == null) {
+            return Integer.MAX_VALUE;
+        }
+        return Math.min(root.data, Math.min(minimum(root.left), minimum(root.right)));
+    }
+
+    public static int maximum(BinaryTreeNode<Integer> root) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        return Math.max(root.data, Math.max(maximum(root.left), maximum(root.right)));
+    }
+
+    public static boolean isBST(BinaryTreeNode<Integer> root) {
+        if (root == null) {
+            return true;
+        }
+        int leftMax = maximum(root.left);
+        int rightMin = minimum(root.right);
+        if (root.data <= leftMax) {
+            return false;
+        }
+
+        if (root.data > rightMin) {
+            return false;
+        }
+        boolean isLeftBST = isBST(root.left);
+        boolean isRightBST = isBST(root.right);
+        if (isLeftBST && isRightBST) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     public static void main(String[] args) {
         BinaryTreeNode<Integer> root = takeInputTreeBetter(true,0,false);
         printTreeDetailed(root);
