94bbdc89cfc7b3f3d4e8f9039dcc19732d8d7a2c
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-19 15:34:40 +0200

    LCA to BST

--- src/BSTI/LL.java
+++ src/BSTI/LL.java
@@ -50,5 +50,21 @@ public class LL {
 
 
     }
+    public static int getLCA(BinaryTreeNode<Integer> root, int a, int b) {
+        if(root==null)
+        {
+            return -1;
+        }
+
+        if(a<root.data && b<root.data)
+        {
+            return getLCA(root.left,a,b);
+        }
+        if(a>root.data && b>root.data)
+        {
+            return getLCA(root.right,a,b);
+        }
+        return root.data;
+    }
 
 }
