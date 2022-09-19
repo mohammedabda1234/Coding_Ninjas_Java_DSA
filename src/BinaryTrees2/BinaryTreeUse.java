7f7d204ec50f5e573dab76511a438c00af16b5be
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-19 15:53:41 +0200

    Print nodes at distance k from node

--- src/BinaryTrees2/BinaryTreeUse.java
+++ src/BinaryTrees2/BinaryTreeUse.java
@@ -554,6 +554,75 @@ public class BinaryTreeUse {
 
 
     }
+    public static void nodesAtDistanceK(BinaryTreeNode<Integer> root, int node, int k) {
+
+        print(root,node,k);
+
+
+    }
+
+    private static int print(BinaryTreeNode<Integer> root, int node, int k)
+    {
+        //If tree is empty return -1
+        if (root==null)
+            return -1;
+
+        int rootData=root.data;
+        if (rootData==node)
+        {
+            printNodesAtDistanceK(root, k);
+            return 0;
+        }
+
+        int leftSubTreeDist=0,rightSubTreeDist=0;
+
+        leftSubTreeDist=print(root.left,node,k);
+        if (leftSubTreeDist!=-1)
+        {
+            if(leftSubTreeDist+1==k)
+            {
+                System.out.println(rootData);
+            }
+            else
+            {
+                rightSubTreeDist=k-(leftSubTreeDist+1)-1;
+                printNodesAtDistanceK(root.right, rightSubTreeDist);
+            }
+            return leftSubTreeDist+1;
+        }
+
+        rightSubTreeDist=print(root.right,node,k);
+        if (rightSubTreeDist!=-1)
+        {
+            if(rightSubTreeDist+1==k)
+            {
+                System.out.println(rootData);
+            }
+            else
+            {
+                leftSubTreeDist=k-(rightSubTreeDist+1)-1;
+                printNodesAtDistanceK(root.left, leftSubTreeDist);
+            }
+            return rightSubTreeDist+1;
+        }
+        return -1;
+    }
+
+    private static void printNodesAtDistanceK(BinaryTreeNode<Integer> root, int k)
+    {
+        if (root==null || k<0)
+            return;
+
+        if (k == 0)
+        {
+            System.out.println(root.data);
+            return;
+        }
+
+        printNodesAtDistanceK(root.left,k-1);
+        printNodesAtDistanceK(root.right,k-1);
+
+    }
     public static void main(String[] args) {
 //        BinaryTreeNode<Integer> root = takeInputTreeBetter(true,1,false);
 //        BinaryTreeNode<Integer> root = takeInputLevelWise();
