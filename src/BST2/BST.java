0401e7776721b05390acb1c15865036fc0f4255b
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-20 06:29:48 +0200

    Structure of BST class

---
+++ src/BST2/BST.java
@@ -0,0 +1,24 @@
+package BST2;
+
+import BSTI.BinaryTreeNode;
+
+public class BST {
+    private BinaryTreeNode<Integer> root;
+    private int size;
+
+    public boolean isPresent(int x){
+        return false;
+    }
+    public void insert(int x){
+
+    }
+    public boolean deleteData(int x){
+        return false;
+    }
+    public int size(){
+        return size;
+    }
+    public void printTree(){
+
+    }
+}
---
+++ src/BST2/BSTUse.java
@@ -0,0 +1,9 @@
+package BST2;
+
+public class BSTUse {
+    public static void main(String[] args) {
+        BST b = new BST();
+        b.insert(3);
+        b.insert(5);
+    }
+}
