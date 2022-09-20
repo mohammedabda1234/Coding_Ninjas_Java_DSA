658cbf28ac269913423ff30a152b8d450d755017
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-20 06:56:49 +0200

    Search and Print BST

--- src/BST2/BST.java
+++ src/BST2/BST.java
@@ -1,13 +1,26 @@
 package BST2;
-
 import BSTI.BinaryTreeNode;
-
 public class BST {
     private BinaryTreeNode<Integer> root;
     private int size;
 
+    private static boolean isPresentHelper(BinaryTreeNode<Integer>node,int x){
+        if (node == null){
+            return false;
+        }
+        if (node.data == x){
+            return true;
+        }
+        if (x < node.data){
+            //call left side
+            return isPresentHelper(node.left,x);
+        }else {
+            //call right side
+            return isPresentHelper(node.right,x);
+        }
+    }
     public boolean isPresent(int x){
-        return false;
+       return isPresentHelper(root,x);
     }
     public void insert(int x){
 
@@ -18,7 +31,22 @@ public class BST {
     public int size(){
         return size;
     }
+    private static void printTreeHelper(BinaryTreeNode<Integer>node){
+        if (node == null){
+            return;
+        }
+        System.out.print(node.data+" : ");
+        if (node.left != null){
+            System.out.print(" L "+node.left.data+" , ");
+        }
+        if (node.right != null){
+            System.out.print(" R "+node.right.data);
+        }
+        System.out.println();
+        printTreeHelper(node.left);
+        printTreeHelper(node.right);
+    }
     public void printTree(){
-
+        printTreeHelper(root);
     }
 }
