ecd29a6bbc6a622774b2168c8c5a7aff4c2bcc90
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-21 06:47:28 +0200

    print tree 2

--- src/Trees/TreeTest.java
+++ src/Trees/TreeTest.java
@@ -11,7 +11,20 @@ public class TreeTest {
             printTree(child);
         }
     }
-
+    public static void printTree1(TreeNode<Integer> root){
+        if (root == null){
+            return;
+        }
+        System.out.print(root.data+" : ");
+        for (int i = 0; i < root.children.size(); i++) {
+            System.out.print(root.children.get(i).data+" ");
+        }
+        System.out.println();
+        for (int i = 0; i < root.children.size(); i++) {
+            TreeNode<Integer>child = root.children.get(i);
+            printTree1(child);
+        }
+    }
     public static void main(String[] args) {
         TreeNode<Integer> root = new TreeNode<>(4);
         TreeNode<Integer> node1 = new TreeNode<>(2);
@@ -26,6 +39,7 @@ public class TreeTest {
         node2.children.add(node4);
         node2.children.add(node5);
         printTree(root);
-
+        System.out.println();
+        printTree1(root);
     }
 }
