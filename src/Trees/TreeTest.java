4c44639337b142ad99726bf17f994e8d3e8ab9bc
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-21 16:56:52 +0200

    Node having sum of children and node is max

--- src/Trees/TreeTest.java
+++ src/Trees/TreeTest.java
@@ -171,6 +171,40 @@ public class TreeTest {
         }
         return false;
     }
+    private static int sum(TreeNode<Integer> root) {
+        if(root==null) {
+            return 0;
+        }
+        int sum= root.data;
+        for(int i=0;i<root.children.size();i++) {
+            sum+=root.children.get(i).data;
+        }
+        return sum;
+    }
+
+    public static TreeNode<Integer> maxSumNode(TreeNode<Integer> root){
+
+        if(root==null) {
+            return null;
+        }
+        int sumRoot = sum(root);
+        int max=Integer.MIN_VALUE;
+        TreeNode<Integer> maxNode = null;
+        for(int i=0;i<root.children.size();i++) {
+
+            TreeNode<Integer> maxNode1 =maxSumNode(root.children.get(i));
+            int sum = sum(maxNode1);
+            if( max < sum ) {
+                max = sum;
+                maxNode = maxNode1;
+            }
+        }
+        if( sumRoot > max ) {
+            return root;
+        }
+        return maxNode;
+
+    }
     public static void main(String[] args) {
         TreeNode<Integer> root = takeInputLevel();
         printTree1(root);
