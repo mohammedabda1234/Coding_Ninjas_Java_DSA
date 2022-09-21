f96c71f9e794b737204a4efc0c8d0a38512c36d5
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-21 16:54:39 +0200

    Check if generic tree contain element x

--- src/Trees/TreeTest.java
+++ src/Trees/TreeTest.java
@@ -157,6 +157,20 @@ public class TreeTest {
         }
         System.out.print(root.data+" ");
     }
+    public static boolean checkIfContainsX(TreeNode<Integer> root, int x){
+        if(root==null) {
+            return false;
+        }
+        if(root.data == x) {
+            return true;
+        }
+        for(int i=0;i<root.children.size();i++) {
+            if( checkIfContainsX(root.children.get(i), x) ) {
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         TreeNode<Integer> root = takeInputLevel();
         printTree1(root);
