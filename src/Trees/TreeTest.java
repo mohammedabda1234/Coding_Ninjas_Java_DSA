205386299ab1f194fff93a688e0f0cbc0056e507
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-21 16:59:06 +0200

    Next larger element

--- src/Trees/TreeTest.java
+++ src/Trees/TreeTest.java
@@ -229,6 +229,30 @@ public class TreeTest {
         }
         return true;
     }
+    public static TreeNode<Integer> findNextLargerNode(TreeNode<Integer> root, int n){
+
+        if(root==null) {
+            return root;
+        }
+        int min=Integer.MAX_VALUE;
+        TreeNode<Integer> minNode = null;
+        for(int i=0;i<root.children.size();i++) {
+
+            TreeNode<Integer> maxNode = findNextLargerNode(root.children.get(i), n);
+            if(maxNode==null) {
+                continue;
+            }
+            int max = maxNode.data;
+            if(min>max) {
+                min = max;
+                minNode = maxNode;
+            }
+        }
+        if( root.data > n && root.data < min ) {
+            return root;
+        }
+        return minNode;
+    }
     public static void main(String[] args) {
         TreeNode<Integer> root = takeInputLevel();
         printTree1(root);
