285c6ef8098e93814faee8c1dfa714d7f3ebafb9
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-21 16:58:10 +0200

    Structurally identical

--- src/Trees/TreeTest.java
+++ src/Trees/TreeTest.java
@@ -205,6 +205,30 @@ public class TreeTest {
         return maxNode;
 
     }
+    public static boolean checkIdentical(TreeNode<Integer> root1, TreeNode<Integer> root2){
+
+
+        if(root1==null && root2==null) {
+            return true;
+        }
+        if((root1==null && root2!=null ) || ( root2==null && root1!=null )) {
+            return false;
+        }
+        if(root1.data != root2.data) {
+            return false;
+        }
+        if(root1.children.size()!=root2.children.size()) {
+            return false;
+        }
+        for(int i=0;i<root1.children.size();i++) {
+
+            if( !checkIdentical(root1.children.get(i),root2.children.get(i))) {
+                return false;
+            }
+
+        }
+        return true;
+    }
     public static void main(String[] args) {
         TreeNode<Integer> root = takeInputLevel();
         printTree1(root);
