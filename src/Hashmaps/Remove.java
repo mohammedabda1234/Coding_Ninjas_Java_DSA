05eebdd42125055646f9eab473ab58abd9c7feb8
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-22 09:29:26 +0200

    Maximum Frequency Number

--- src/Hashmaps/Remove.java
+++ src/Hashmaps/Remove.java
@@ -4,10 +4,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Remove {
+    public static int maxFrequencyNumber(int[] arr){
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for(int item:arr) {
+            //we are going through each item of the array and put its frequency as value .and item in array as key
+            if(hashMap.containsKey(item)) {
+                hashMap.put(item, hashMap.get(item)+1);
+            }else {
+                hashMap.put(item, 1);
+            }
+            //hashMap.put(item, hashMap.getOrDefault(item, 0)+1)
+        }
+        int max = 0, ans = Integer.MIN_VALUE;
+        for(int item:arr)
+        {
+            if(hashMap.get(item) > max)
+            {
+                max = hashMap.get(item);
+                ans = item;
+            }
+        }
+        return ans;
+
+    }
     public static void main(String[] args) {
         int[] arr = {1,3,2,2,3,4,5,6,7,8,9,9,9,};
         ArrayList<Integer> out = removeDuplicate(arr);
         System.out.println(out);
+        System.out.println(maxFrequencyNumber(arr));
     }
 
     private static ArrayList<Integer> removeDuplicate(int[] arr) {
