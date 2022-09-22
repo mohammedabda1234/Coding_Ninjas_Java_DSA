16cc60ad26bd452946d5efc865a6d0cee55237b7
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-22 13:53:57 +0200

    Pair Sum to 0

--- src/Hashmaps/ArrayInter.java
+++ src/Hashmaps/ArrayInter.java
@@ -3,6 +3,37 @@ package Hashmaps;
 import java.util.HashMap;
 
 public class ArrayInter {
+    public static int PairSum(int[] input, int size) {
+        if (size==0)
+            return 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int key:input)
+        {
+            if(map.containsKey(key))
+            {
+                map.put(key,map.get(key)+1);
+            }
+            else
+            {
+                map.put(key,1);
+            }
+        }
+        int countPairs=0;
+        for (Integer i: map.keySet())
+        {
+            if (map.containsKey(-i) && i!=0)
+            {
+                countPairs=countPairs+(map.get(i)*map.get(-i));
+            }
+        }
+        countPairs=countPairs/2;
+        if (map.containsKey(0))
+        {
+            int val=map.get(0);
+            countPairs=countPairs+(val*(val-1))/2;
+        }
+        return countPairs;
+    }
     public static void main(String[] args) {
         int[] arr1 = {3,4,5,6,3,2,5,0};
         int[] arr2 = {5,6,8,9,2,3,0,1};
