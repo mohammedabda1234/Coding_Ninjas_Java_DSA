1eb7e3592d4a1bebabbafe228f40c6a0a9dbbb03
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-23 17:21:32 +0200

    Pairs with difference K

--- src/Hashmaps/Unique.java
+++ src/Hashmaps/Unique.java
@@ -55,6 +55,34 @@ public class Unique {
         return ans;
 
     }
+    public static int getPairsWithDifferenceK(int arr[], int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int pairCount = 0;
+        for(int item:arr)
+        {
+            int p1 = item + k;
+            boolean flag = false;
+            if(item == p1)
+            {
+                flag = true; //this case is there for k=0
+            }
+            if(map.containsKey(p1))
+            {
+                pairCount += map.get(p1);
+            }
+            int p2 = item - k;
+            if(map.containsKey(p2) && !flag)
+            {
+                pairCount += map.get(p2);
+            }
+            if(map.containsKey(item)) {
+                map.put(item, map.get(item)+1);
+            }else {
+                map.put(item, 1);
+            }
+        }
+        return pairCount;
+    }
     public static void main(String[] args) {
 
     }
