fe07b7711f29ee2d9717daf089015751bac04422
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-23 17:20:35 +0200

    Longest Consecutive Sequence

--- src/Hashmaps/Unique.java
+++ src/Hashmaps/Unique.java
@@ -1,5 +1,6 @@
 package Hashmaps;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Unique {
@@ -18,6 +19,41 @@ public class Unique {
             }
         }
         return ans;
+    }
+    public static ArrayList<Integer> longestConsecutiveIncreasingSequence(int[] arr) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i =0;i<arr.length;i++)
+        {
+            map.put(arr[i],1);
+        }
+
+        int overallcount=0,maxStart=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++)
+        {
+
+            if(!map.containsKey(arr[i]-1))
+            {
+                int currentcount = 1;
+                int currentstart = arr[i];
+
+                while(map.containsKey(currentstart+1))
+                {
+                    currentcount++;
+                    currentstart=currentstart+1;
+                }
+                if(currentcount>overallcount)
+                {
+                    overallcount=currentcount;
+                    maxStart = arr[i];
+                }
+            }
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        ans.add(maxStart);
+        ans.add(maxStart+overallcount-1);
+
+        return ans;
+
     }
     public static void main(String[] args) {
 
