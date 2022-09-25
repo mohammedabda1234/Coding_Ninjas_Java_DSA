5e02480a668c88b40d22aa70ab0781ab65e7d61a
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-25 15:51:57 +0200

    remove minimum from PQ

--- src/PriorityQueues1/Priority_Queues.java
+++ src/PriorityQueues1/Priority_Queues.java
@@ -33,8 +33,38 @@ public class Priority_Queues<T> {
         }
         return heap.get(0).value;
     }
-    public T removeMin(){
+    public T removeMin() throws PriorityQueuesException {
+        if (isEmpty()){
+            throw new PriorityQueuesException();
+        }
+        Element<T> remove = heap.get(0);
+        T ans = remove.value;
+        heap.set(0,heap.get(heap.size()-1));
+        heap.remove(heap.size()-1);
+        int parentIndex = 0;
+        int leftIndex = 2 * parentIndex +1;
+        int rightIndex = 2 * parentIndex +2;
 
+        while (leftIndex < heap.size()) {
+            int minIndex = parentIndex;
+            if (heap.get(leftIndex).priority < heap.get(minIndex).priority) ;
+            minIndex = leftIndex;
+
+            if (rightIndex < heap.size() && (heap.get(rightIndex).priority < heap.get(minIndex).priority)) ;
+            minIndex = rightIndex;
+
+            if (minIndex == parentIndex){
+                break;
+            }
+
+            Element<T> temp = heap.get(minIndex);
+            heap.set(minIndex, heap.get(parentIndex));
+            heap.set(parentIndex, temp);
+            parentIndex =minIndex;
+            leftIndex = 2 * parentIndex + 1;
+            rightIndex = 2 * parentIndex + 2;
+        }
+        return ans;
     }
     public int size(){
         return heap.size();
