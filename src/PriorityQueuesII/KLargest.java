763cff504c82d50b33fb0498247dafeae327b80e
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-26 08:44:44 +0200

    print k smallest element

--- src/PriorityQueuesII/KLargest.java
+++ src/PriorityQueuesII/KLargest.java
@@ -1,5 +1,7 @@
 package PriorityQueuesII;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class KLargest {
@@ -19,8 +21,39 @@ public class KLargest {
             System.out.println(pq.poll());
         }
     }
+    public static ArrayList<Integer> kSmallest(int n, int[] input, int k) {
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i =0;i<k;i++)
+        {
+            pq.add(input[i]);
+        }
+
+        for(int i =k;i<n;i++)
+        {
+
+            if(pq.peek()>input[i])
+            {
+                pq.poll();
+                pq.add(input[i]);
+            }
+        }
+        while(!pq.isEmpty())
+        {
+            ans.add(pq.peek());
+            pq.poll();
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int[] arr  = {6,7,3,1,2,9,0,4,5,8};
-        printKLargest(arr,3);
+        int k = 3;
+        int n = arr.length -1;
+        printKLargest(arr,k);
+        ArrayList<Integer> out = kSmallest(n,arr,k);
+        for (int i :out) {
+            System.out.println(i);
+        }
     }
 }
