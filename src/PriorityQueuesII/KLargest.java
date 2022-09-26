2c6c18e5182918c6c3abec62eacf4f78d5ad5d31
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-26 08:49:59 +0200

    Kth largest element

--- src/PriorityQueuesII/KLargest.java
+++ src/PriorityQueuesII/KLargest.java
@@ -23,6 +23,19 @@ public class KLargest {
         }
         return true;
     }
+    public static int kthLargest(int n, int[] arr, int k) {
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for(int i=0;i<k;i++)
+            priorityQueue.add(arr[i]);
+        for(int i= k ;i < arr.length;i++) {
+            if(arr[i] > priorityQueue.peek() ) {
+                priorityQueue.remove();
+                priorityQueue.add(arr[i]);
+            }
+        }
+        return priorityQueue.remove();
+    }
     public static void printKLargest(int[] arr, int k) {
        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
