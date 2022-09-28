40bdcb784d20f1d1ab7aba1782d52eea03984f12
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-28 18:32:57 +0200

    Print Permutations of a String

--- src/Recursion3/Print.java
+++ src/Recursion3/Print.java
@@ -94,6 +94,19 @@ public class Print {
 
         return output;
     }
+    public static void permutations(String input){
+        // Write your code here
+        permutationsHelper(input,"");
+
+    }
+    private static void permutationsHelper(String input, String output)
+    {
+        if (input.length()==0)
+            System.out.println(output);
+
+        for (int i=0;i<input.length();i++)
+            permutationsHelper(input.substring(0,i)+input.substring(i+1), output+input.charAt(i));
+    }
     public static void main(String[] args) {
 
     }
