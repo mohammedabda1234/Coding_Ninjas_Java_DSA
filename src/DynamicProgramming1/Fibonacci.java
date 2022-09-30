f668ab96b488c83b35f1ca6866e74e7d2accbeb0
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-30 06:37:15 +0200

    Iterative DP

--- src/DynamicProgramming1/Fibonacci.java
+++ src/DynamicProgramming1/Fibonacci.java
@@ -1,16 +1,19 @@
 package DynamicProgramming1;
-
 import java.util.Scanner;
 
 public class Fibonacci {
-    public static int fibb(int n){
-        if (n == 0 || n == 1){
-            return n;
-        }
-        int ans1 = fibb(n -1);
-        int ans2 = fibb(n -2);
-        return ans2 + ans1;
-    }
+   public static int fib(int n){
+       if (n == 0 || n == 1){
+           return n;
+       }
+       int[] dp = new int[n+1];
+       dp[0] = 0;
+       dp[1] = 1;
+       for (int i = 2; i <= n ; i++) {
+           dp[i] = dp[i-1]+ dp[i-2];
+       }
+       return dp[n];
+   }
     public static int fib(int n,int[] dp){
         if (n == 0 || n ==1){
             return n;
@@ -38,5 +41,6 @@ public class Fibonacci {
             dp[i] = -1;
         }
         System.out.println(fib(n,dp));
+        System.out.println(fib(n));
     }
 }
