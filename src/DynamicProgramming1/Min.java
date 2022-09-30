f7282783df10c78d312ada27ca005b02a6de18f7
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-09-30 14:51:52 +0200

    minimum square Iterative to represent n

--- src/DynamicProgramming1/Min.java
+++ src/DynamicProgramming1/Min.java
@@ -17,16 +17,33 @@ public class Min {
                 if (min > current) {
                     min = current;
                 }
-
         }
         return min+1;
     }
+    public static int minSquare(int n){
+        if (n == 0){
+            return 0;
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j*j <= i; j++) {
+                int current = dp[i -(j*j)];
+                if (min>current){
+                    min =current;
+                }
+            }
+            dp[i] = min+1;
+        }
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n = 41;
-        int[] dp = new int[n+1];
-        for (int i = 0; i <= n; i++) {
-            dp[i] = -1;
-        }
-        System.out.println(minSquare(n,dp));
+//        int[] dp = new int[n+1];
+//        for (int i = 0; i <= n; i++) {
+//            dp[i] = -1;
+//        }
+        System.out.println(minSquare(n));
     }
 }
