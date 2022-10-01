d129a44661c62d91005931ebad3fbb6fa360569c
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-10-01 15:41:23 +0200

    Subset Sum

--- src/DynamicProgramming2/Ass.java
+++ src/DynamicProgramming2/Ass.java
@@ -114,6 +114,28 @@ public class Ass {
         }
         return dp[0][0];
     }
+    static boolean isSubsetPresent(int[] set, int n, int sum) {
+        boolean subset[][] = new boolean[sum + 1][n + 1];
+        // If sum is 0, then answer is true
+        for (int i = 0; i <= n; i++)
+            subset[0][i] = true;
+
+        // If sum is not 0 and set is empty,
+        // then answer is false
+        for (int i = 1; i <= sum; i++)
+            subset[i][0] = false;
+        // Fill the subset table in bottom
+        // up manner
+        for (int i = 1; i <= sum; i++) {
+            for (int j = 1; j <= n; j++) {
+                subset[i][j] = subset[i][j - 1];
+                if (i >= set[j - 1])
+                    subset[i][j] = subset[i][j]
+                            || subset[i - set[j - 1]][j - 1];
+            }
+        }
+        return subset[sum][n];
+    }
     public static void main(String[] args) {
 
     }
