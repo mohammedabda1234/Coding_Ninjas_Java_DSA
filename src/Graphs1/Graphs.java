d7878ef28f2e73a619cdf71923f24585dbd3110c
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-10-03 09:22:41 +0200

    breath first traversal

--- src/Graphs1/Graphs.java
+++ src/Graphs1/Graphs.java
@@ -1,5 +1,7 @@
 package Graphs1;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Graphs {
@@ -8,7 +10,6 @@ public class Graphs {
         boolean[] visited = new boolean[adjMatrix.length];
         dfTraversal(adjMatrix,0,visited);
     }
-
     private static void dfTraversal(int[][] adjMatrix, int currentVertex,boolean[] visited) {
         visited[currentVertex] = true;
         System.out.print(currentVertex+" ");
@@ -19,6 +20,22 @@ public class Graphs {
         }
     }
 
+    public static void bfTraversal(int[][] adjMatrix){
+        Queue<Integer> pending = new LinkedList<>();
+        boolean[] visited = new boolean[adjMatrix.length];
+        visited[0] = true;
+        pending.add(0);
+        while (!pending.isEmpty()){
+            int currentVertex = pending.poll();
+            System.out.print(currentVertex+" ");
+            for (int i = 0; i < adjMatrix.length; i++) {
+                if (adjMatrix[currentVertex][i] == 1 && !visited[i]){
+                    pending.add(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n= s.nextInt();
@@ -37,5 +54,8 @@ public class Graphs {
 //            System.out.println();
 //        }
         dfTraversal(adjMatrix);
+        System.out.println();
+        System.out.println("breath first traversal");
+        bfTraversal(adjMatrix);
     }
 }
