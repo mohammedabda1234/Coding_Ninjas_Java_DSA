ffc7ca4d74fbc5ae249dcb9ff268d54035a3d7bc
mohammedabda1234 <mohammedabdalla15@hotmail.com>
2022-10-03 09:06:23 +0200

    Depth first traversal

--- src/Graphs1/Graphs.java
+++ src/Graphs1/Graphs.java
@@ -3,6 +3,22 @@ package Graphs1;
 import java.util.Scanner;
 
 public class Graphs {
+
+    public static void dfTraversal(int[][] adjMatrix){//pre order traversal
+        boolean[] visited = new boolean[adjMatrix.length];
+        dfTraversal(adjMatrix,0,visited);
+    }
+
+    private static void dfTraversal(int[][] adjMatrix, int currentVertex,boolean[] visited) {
+        visited[currentVertex] = true;
+        System.out.print(currentVertex+" ");
+        for (int i = 0; i < adjMatrix.length; i++) {
+            if (adjMatrix[currentVertex][i] == 1 && visited[i] == false){
+                dfTraversal(adjMatrix,i,visited);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n= s.nextInt();
@@ -14,11 +30,12 @@ public class Graphs {
             adjMatrix[V1][V2] = 1;
             adjMatrix[V2][V1] = 1;
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(adjMatrix[i][j]+" ");
-            }
-            System.out.println();
-        }
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                System.out.print(adjMatrix[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+        dfTraversal(adjMatrix);
     }
 }
